8075b4685cfb5ab5984e0e2279f85e7d02fce987
fredrikfarre <fredrikfarre@gmail.com>
2019-08-23 10:48:40 +0200

    test

---
+++ Address_book/src/org/addressbook/main/SimpleMain.java
@@ -0,0 +1,37 @@
+/*
+import java.lang.*;
+package org.addressbook.main;
+
+class SimpleMain{
+    private int age;
+
+    SimpleMain(int age){
+        this.age = age;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public boolean isOlder( SimpleMain person ){
+        System.out.println( this.getAge().compareTo(person.getAge()) );
+        if( getAge() > person.getAge() ){
+            return true;
+        }
+        return false;
+    }
+}
+
+
+
+class Main{
+    public static void main(String[] args) {
+        SimpleMain simon = new SimpleMain(28);
+        SimpleMain fredrik = new SimpleMain(38);
+
+        System.out.println(simon.isOlder(fredrik));
+
+
+    }
+}
+*/
